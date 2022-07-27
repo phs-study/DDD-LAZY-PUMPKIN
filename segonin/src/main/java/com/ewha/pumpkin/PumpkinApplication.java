f7cac187257f8a2af1ba4e2cf48d3fd9816553f3
ksi0921 <ksi0921@jainwon.com>
2022-07-27 17:41:58 +0900

    feat: modulize to personal project

---
+++ segonin/src/main/java/com/ewha/pumpkin/PumpkinApplication.java
@@ -0,0 +1,13 @@
+package com.ewha.pumpkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumpkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PumpkinApplication.class, args);
+	}
+
+}
