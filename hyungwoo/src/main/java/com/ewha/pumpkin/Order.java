2add36bd4b06303b0f6ad94c1938220858ba99f7
khw0801 <khw0801@jainwon.com>
2022-08-24 13:44:37 +0900

    feat : 주문 도메인 모델 패턴 구현

--- hyungwoo/src/main/java/com/ewha/pumpkin/Order.java
+++ hyungwoo/src/main/java/com/ewha/pumpkin/Order.java
@@ -1,14 +1,61 @@
 package com.ewha.pumpkin;
 
+import java.util.List;
+
 public class Order {
     private OrderState state;
     private ShippingInfo shippingInfo;
+    private List<OrderLine> orderLines;
+    private Money totalAmounts;
+
+    public Order(List<OrderLine> orderLines, ShippingInfo shippingInfo) {
+        setOrderLines(orderLines);
+        setShippingInfo(shippingInfo); // (5)
+    }
+
+    private void setShippingInfo(ShippingInfo shippingInfo) {
+        if (shippingInfo == null) {
+            throw new IllegalArgumentException("no ShippingInfo");
+        }
+        this.shippingInfo = shippingInfo;
+    }
+
+    private void setOrderLines(List<OrderLine> orderLines) {
+        verifyLeastOneOrMoreOrderLines(orderLines); // (1)
+        this.orderLines = orderLines;
+        calculateTotalAmounts();
+    }
+
+    private void calculateTotalAmounts() { // (3)
+        int sum = orderLines.stream()
+                .mapToInt(OrderLine::getAmounts)
+                .sum();
+        this.totalAmounts = new Money(sum);
+    }
+
+    private void verifyLeastOneOrMoreOrderLines(List<OrderLine> orderLines) {
+        if (orderLines == null || orderLines.isEmpty()) {
+            throw new IllegalArgumentException("no OrderLine");
+        }
+    }
 
     public void changeShippingInfo(ShippingInfo newShippingInfo) {
+        verifyNotYetShipped();
         if(!isShippingChangeable()) {
             throw new IllegalArgumentException("can't change shipping in " + state);
         }
         this.shippingInfo = newShippingInfo;
+    }
+
+    public void cancel() {
+        verifyNotYetShipped();
+        this.state = OrderState.CANCELED;
+    }
+
+    private void verifyNotYetShipped() {
+        if (state != OrderState.PAYMENT_WAITING && state != OrderState.PREPARING) { // (7), (8)
+            throw new IllegalArgumentException("already shipped");
+        }
     }
 
     private boolean isShippingChangeable() {
---
+++ hyungwoo/src/main/java/com/ewha/pumpkin/OrderLine.java
@@ -0,0 +1,23 @@
+package com.ewha.pumpkin;
+
+public class OrderLine {
+    private Product product;
+    private int price;
+    private int quantity; // (2)
+    private int amounts;
+
+    public OrderLine(Product product, int price, int quantity, int amounts) {
+        this.product = product;
+        this.price = price;
+        this.quantity = quantity;
+        this.amounts = amounts;
+    }
+
+    private int calculateAmounts() { // (4)
+        return price * quantity;
+    }
+
+    public int getAmounts() {
+        return amounts;
+    }
+}
